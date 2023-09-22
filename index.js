7cd50c82c59945a42045d50d319d6479aa798c56
David <618375+dangermouse121985@users.noreply.github.com>
2023-09-21 22:34:11 -0400

    Added Responses to each of the Endpoints

--- index.js
+++ index.js
@@ -50,8 +50,20 @@ let moviesDB = [
     }
 ]
 
-let genres = [];
-let directors = [];
+let genres = [
+    {
+        title: 'action',
+        description: 'Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.'
+    }
+];
+let directors = [
+    {
+        name: "Christopher Nolan",
+        bio: "Christopher Edward Nolan CBE is a British and American filmmaker. Known for his Hollywood blockbusters with complex storytelling, Nolan is considered a leading filmmaker of the 21st century. His films have grossed $5 billion worldwide.",
+        birthYear: 1970,
+        deathYear: null
+    }
+];
 
 app.use(morgan('combined', {stream: accessLogStream}));
 app.use(express.static('public'));
@@ -65,7 +77,7 @@ app.get('/movies/:title', (req, res) => {
     { return movie.title === req.params.title }));
 });
 
-app.get('/genre/:title', (req, res) => {
+app.get('/genres/:title', (req, res) => {
     res.json(genres.find((genre) => 
     { return genre.title === req.params.title }));
 });
@@ -80,11 +92,15 @@ app.post('/users', (reg,res) => {
 });
 
 app.put('/users/:username', (req, res) => {
-    res.send('Successful PUT request upadating user\' username');
+    res.send('Successful PUT request upadating user\'s username');
 });
 
-app.delete('/favorites/:movie-id', (req, res) => {
-    res.send('Successful DELETE request removeing movie from user\'s favorites movie list');
+app.put('/users/:username/favorites/:movieId', (req, res) => {
+    res.send('Successful PUT request adding movie to user\'s favorites list');
+});
+
+app.delete('/users/:username/favorites/:movie-id', (req, res) => {
+    res.send('Successful DELETE request removing movie from user\'s favorites movie list');
 });
 
 app.delete('/users/:username', (req, res) => {
@@ -92,7 +108,7 @@ app.delete('/users/:username', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Movie Flix')
+    res.send('Welcome to Movie Flix');
 });
 
 app.use((err, req, res, next) => {
