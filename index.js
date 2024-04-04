3b7bded2c326cb571ed8a2ffc71edb950cbc4a99
David <618375+dangermouse121985@users.noreply.github.com>
2024-04-04 18:35:37 -0400

    added localhost:4020 to cors policy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://dacflix.netlify.app'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://localhost:4020', 'https://dacflix.netlify.app'];
 
 app.use(cors({
     origin: (origin, callback) => {
