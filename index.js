f8ab1e0a024040874e13c5a0781d8344db4ebb2b
Ubuntu <ubuntu@ip-172-31-41-127.ec2.internal>
2024-07-18 23:05:09 +0000

    updated to point to db on aws server

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ const passport = require('passport');
 require('./passport');
 
 
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/myflixDB', { useNewUrlParser: true, useUnifiedTopology: true});
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
 
